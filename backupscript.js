96a68e44db078709de516048528bdf915bcfd1e6
shreya maheshwari <62559289+shreya12-37@users.noreply.github.com>
2021-06-28 21:39:19 +0530

    Update backupscript.js

--- backupscript.js
+++ backupscript.js
@@ -18,22 +18,59 @@ frappe.listview_settings['Dispatch Details'] = {
              name = name.replace("/","%2F");
              url = "http://rtmills.onehash.ai/api/method/frappe.utils.print_format.download_pdf?doctype=Dispatch%20Details&name="+name+"&format=Standard&no_letterhead=0&letterhead=RT%20MILLS%20Letter%20Head&settings=%7B%7D&_lang=en-US";
             //  window.open(url);
+            // console.log(url);
+            publicdocument(url,"Dispatch Details",name);
              
          }
+        // console.log(url);
         //  console.log(docnames);
         //console.log(frm.attachments);
-        function publicdocument(url, Dispatch Details, Dispatch_details, Dispatch_details)
-        {
+        function publicdocument(url, doctype, docname) {
+            console.log("url",url,"doctype",doctype,"docname", docname);
             var blob = null;
             var xhr = new XMLHttpRequest();
             xhr.open("GET", url);
             var splittedParts = url.split("/");
-            var splitByQuestionMark = splittedParts[6].split('?');
-            filename = splitByQuestionMark[0];
-            xhr.responseType = "blob";
+            var splitByQuestionMark = splittedParts[5].split('?');
+            var filename = splitByQuestionMark[0];
+            console.log("file name",filename,"doctype",doctype,"docname",docname);
             
+            xhr.responseType = "blob"; //force the HTTP response, response-type header to be blob
+            xhr.onload = function() {
+                blob = xhr.response; //xhr.response is now a blob object
+                var reader = new FileReader();
+                console.log("File Reader",reader);
+                reader.readAsDataURL(blob);
+                reader.onloadend = function() {
+                    var base64data = reader.result;
+                    console.log("ye base64data mila", base64data);
+                    frappe.call({
+                        method: "uploadfile",
+                        type: 'POST',
+                        args: {
+                            doctype:doctype,
+                            docname:docname,
+                            filename:filename,
+                            filedata:base64data,
+                            from_form: 1
+                        },
+                        public: true,
+                        async: false,
+                        callback: function(r) {
+                            console.log("file name",filename,"doctype",doctype,"docname",docname);
+                            console.log("KYC document uploaded Succesfully",r);
+                            // frappe.call
+                        },
+                        error: function(data) {
+                            console.log("Inside Error",data);
+                        }
+                        
+                    });
+                };
+            };
+            xhr.send();
         }
-        // frappe.call
+        // publicdocument(url,"Dispatch Details","Dispatch_details");
     }
     
     );
