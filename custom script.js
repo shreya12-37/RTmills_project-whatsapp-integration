c313d25575e3dd97724a3bf8484215dac4ab15e0
shreya maheshwari <62559289+shreya12-37@users.noreply.github.com>
2021-07-12 13:53:01 +0530

    custom calculations

--- custom script.js
+++ custom script.js
@@ -1,1 +1,14 @@
-//
+frappe.ui.form.on('Dispatch Details', {
+     before_save:function(frm){
+	        let product_list = frm.doc.products;
+	        let total_taxable_value=0;
+	        let total_tax_applied=0;
+	        
+	        product_list.forEach(function(product, index){
+	            total_taxable_value = total_taxable_value + parseFloat(product.ass_val);
+	            total_tax_applied = total_tax_applied + parseFloat(product.igst);
+	        });
+	        frm.set_value("total_tax",total_tax_applied);
+	        frm.set_value("total_value",total_taxable_value);
+	    }
+})
