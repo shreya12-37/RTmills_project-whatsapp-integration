6685f7d0b06e96eccf4533de5866637808578a3c
shreya maheshwari <62559289+shreya12-37@users.noreply.github.com>
2021-07-16 15:02:48 +0530

    server script for calculations on form

--- calculation.py
+++ calculation.py
@@ -1,1 +1,10 @@
-
+#doctype event
+final_gst=0;
+amt=0;
+for i in range(len(doc.product_details)):
+    final_gst = float(doc.product_details[i].igst) + float(doc.product_details[i].cgst) + float(doc.product_details[i].sgst)
+    amt = float(doc.product_details[i].amount)
+# frappe.msgprint(amt)
+# frappe.msgprint(final_gst)
+doc.taxable_amount = amt 
+doc.total_gst = final_gst
